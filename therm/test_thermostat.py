1f9987c5894db4960e4115cc22996c832a92bafe
Mike Claffey <mikeclaffey@yahoo.com>
2016-02-15 21:36:39 -0500

    test thermostat - many tests working now

--- therm/test_thermostat.py
+++ therm/test_thermostat.py
@@ -14,6 +14,18 @@ class TestThermometer(object):
         self.temperature = None
 
 
+class FailHelper(TestCase):
+    """This is a kludge to be able to call unittest.TestCase.fail() from outside the class.
+
+    Usage: FailHelper().do_fail("I'm failing because...")
+    """
+    def runTest(self):
+        pass
+
+    def do_fail(self, msg):
+        self.fail(msg)
+
+
 class TestHeater(object):
     def __init__(self):
         self.exception_if_turned_on = True
@@ -23,12 +35,15 @@ class TestHeater(object):
     def set_to_on(self, val=True):
         if val:
             if self.exception_if_turned_on:
-                raise ThermostatException("Heater not allowed to turn on now")
+                FailHelper().do_fail("Heater not allowed to turn on now")
             self._is_turned_on = True
         else:
             if self.exception_if_turned_off:
-                raise ThermostatException("Heater not allowed to turn off now")
+                FailHelper().do_fail("Heater not allowed to turn off now")
             self._is_turned_on = False
+
+    def is_on(self):
+        return self._is_turned_on
 
     def iterate(self):
         pass
@@ -41,41 +56,46 @@ class TestThermostat(TestCase):
         random.seed(0)
 
     def setUp(self):
+        # create instances
         self.heater = TestHeater()
         self.thermometer = TestThermometer()
         self.clock = TestClock()
         self.thermostat = Thermostat(self.heater, self.thermometer, self.clock)
 
+        # setup testing environment
         # in the beginning, temperature is above threshold, heater is off, both exceptions are on
         self.clock.set_clock(0)
         self.thermometer.temperature = 68
-        self.thermostat.threshold_low = 67  # turn on heater if 67 or below
-        self.thermostat.threshold_high = 69  # turn off heater if 69 or above
         self.heater._is_turned_on = False
         self.heater.exception_if_turned_on = True
         self.heater.exception_if_turned_off = True
-        self.thermostat.iterate()
+
+        # start thermostat in target mode with target=68 (thresholds of 67/69)
+        self.thermostat.set_target_temperature(68)
+        self.thermostat.set_mode('target')
+
 
     def test_read_heater_status(self):
+        # turn on
+        self.heater.exception_if_turned_on = False
         self.heater.set_to_on(True)
         self.assertTrue(self.thermostat.get_heater_is_on())
+        # turn off
+        self.heater.exception_if_turned_off = False
         self.heater.set_to_on(False)
         self.assertFalse(self.thermostat.get_heater_is_on())
 
     def test_turn_on_heater_manually(self):
-        # turn on heater
-        self.heater.set_to_on(False)
+        # turn on
         self.heater.exception_if_turned_on = False
-        self.heater.exception_if_turned_off = True
-        self.thermostat.set_heater_to_on(True)
-        self.assertTrue(self.heater._is_turned_on)
+        self.thermostat.set_mode("on")
+        self.assertTrue(self.heater.is_on())
 
     def test_turn_off_heater_manually(self):
         # turn off heater
-        self.heater.exception_if_turned_on = True
         self.heater.exception_if_turned_off = False
-        self.thermostat.set_heater_to_on(False)
-        self.assertFalse(self.heater._is_turned_on)
+        self.thermostat.set_mode("off")
+        self.assertFalse(self.heater.is_on())
 
     def test_read_thermometer(self):
         for temp in [0, -5, 98.6]:
@@ -100,10 +120,10 @@ class TestThermostat(TestCase):
             self.thermostat.set_target_temperature(temp)
 
     def test_turn_heater_on_when_too_cold(self):
-        # after 5 minutes below at of below .threshold_low(), turns on heater
+        # after 5 minutes below at or below .threshold_low(), turns on heater
         # advance to some arbitrary time, go below threshold
         self.clock.advance_random()
-        self.thermometer.temperature = 67
+        self.thermometer.temperature = 66
         self.thermostat.iterate()
         # advance less than the required time, heater still off
         self.clock.advance(minutes=4.9)
@@ -112,19 +132,19 @@ class TestThermostat(TestCase):
         self.clock.advance(minutes=0.1)
         self.heater.exception_if_turned_on = False
         self.thermostat.iterate()
-        self.assertTrue(self.heater._is_turned_on)
+        self.assertTrue(self.heater.is_on())
 
     def helper_get_cold_to_trigger_heater(self):
         # possible unintended consequences if not run as first line in test
         # advance to some arbitrary time, it is now too cold
         self.clock.advance_random()
-        self.thermometer.temperature = 67
+        self.thermometer.temperature = 66
         self.thermostat.iterate()
         # stay cold long enough for heater to kick on
         self.clock.advance(minutes=5)
         self.heater.exception_if_turned_on = False
         self.thermostat.iterate()
-        self.assertTrue(self.heater._is_turned_on)
+        self.assertTrue(self.heater.is_on())
 
     def test_turn_heater_on_when_too_cold_helper(self):
         self.helper_get_cold_to_trigger_heater()
@@ -133,7 +153,7 @@ class TestThermostat(TestCase):
         # threshold duration is 5 minutes. heater should not turn on if cold for 4, warm for 1, cold for 4
         # advance to some arbitrary time, go below threshold
         self.clock.advance_random()
-        self.thermometer.temperature = 67
+        self.thermometer.temperature = 66
         self.thermostat.iterate()
         # advance less than the required time, heater still off
         self.clock.advance(minutes=4)
@@ -144,7 +164,7 @@ class TestThermostat(TestCase):
         self.thermostat.iterate()
         # advance 1 minute, temperature back below threshold
         self.clock.advance(minutes=1)
-        self.thermometer.temperature = 67
+        self.thermometer.temperature = 66
         self.thermostat.iterate()
         # advance less than the required time, heater still off
         self.clock.advance(minutes=4)
@@ -156,19 +176,22 @@ class TestThermostat(TestCase):
         # this arbitrary time must be less than the maximum-on-duration (20 minutes)
         self.clock.advance(minutes=7.2)
         self.thermostat.iterate()
-        # come above threshold, heater should turn off
+        # come above threshold, heater will run until above for 5 minutes
         # total arbitrary time must be less than the maximum-on-duration (20 minutes)
         self.clock.advance(minutes=4.8)
-        self.thermometer.temperature(69)
+        self.thermometer.temperature = 70
+        self.thermostat.iterate()
+        # advance 5 minutes
+        self.clock.advance(minutes=5)
         self.heater.exception_if_turned_off = False
         self.thermostat.iterate()
-        self.assertFalse(self.heater._is_turned_on)
+        self.assertFalse(self.heater.is_on())
 
     def test_dont_cycle_off_heater_too_quickly(self):
         self.helper_get_cold_to_trigger_heater()
         # advance 1 minute, already warm enough, but don't cycle off heater yet
         self.clock.advance(minutes=1)
-        self.thermometer.temperature(69)
+        self.thermometer.temperature = 70
         self.thermostat.iterate()
         # advance to just under cycle minimum time on, heater should still be on
         self.clock.advance(minutes=3.9)
@@ -177,23 +200,22 @@ class TestThermostat(TestCase):
         self.clock.advance(minutes=0.1)
         self.heater.exception_if_turned_off = False
         self.thermostat.iterate()
-        self.assertFalse(self.heater._is_turned_on)
-
+        self.assertFalse(self.heater.is_on())
 
     def test_dont_cycle_on_heater_too_quickly(self):
         # the beginning of this test needs to trigger the heater and let things warm up
         self.helper_get_cold_to_trigger_heater()
         # advance and warm up
         self.clock.advance(minutes=10)
-        self.thermometer.temperature(69)
+        self.thermometer.temperature = 70
         self.heater.exception_if_turned_off = False
         self.thermostat.iterate()
-        self.assertFalse(self.heater._is_turned_on)
+        self.assertFalse(self.heater.is_on())
         # the heater is off now, but throw exception if thermostat tries to turn if off again
         self.heater.exception_if_turned_off = True
         # now quickly get cold again, but don't turn on heater yet
         self.clock.advance(minutes=1)
-        self.thermometer.temperature = 67
+        self.thermometer.temperature = 66
         self.thermostat.iterate()
         # advance to just inside cycle limit, still not on yet
         self.clock.advance(minutes=3.9)
@@ -202,17 +224,16 @@ class TestThermostat(TestCase):
         self.clock.advance(minutes=0.1)
         self.heater.exception_if_turned_on = False
         self.thermostat.iterate()
-        self.assertTrue(self.heater._is_turned_on)
-
+        self.assertTrue(self.heater.is_on())
 
     def test_dont_keep_heater_on_forever(self):
-        # maximum on for 30 minutes, even if not warm enough yet
+        # maximum on for 60 minutes, even if not warm enough yet
         self.helper_get_cold_to_trigger_heater()
         # temperature is still too cool, advance to just before time limit
-        self.clock.advance(minutes=29)
+        self.clock.advance(minutes=59)
         self.thermostat.iterate()
         # advance past limit, heater should go off even though still cold
         self.clock.advance(minutes=1)
         self.heater.exception_if_turned_off = False
         self.thermostat.iterate()
-        self.assertFalse(self.heater._is_turned_on)
+        self.assertFalse(self.heater.is_on())
--- therm/thermostat.py
+++ therm/thermostat.py
@@ -1,32 +1,61 @@
+import logging
+
+from helpers import ThermostatException, Clock
+from heater import HeaterControl
+
+
 class Thermostat(object):
-    thermometer = None
-    clock = None
+    available_modes = ['off', 'on', 'target']
+    current_mode = 'off'
+    _logger = logging.getLogger(__name__)
+    target_temp = None
+    # Actions occur when temperature exceeds thresholds, not simply when it reaches the threshold.
+    # For example, if low threshold is 67, heater will kick on when temperature hits 66 or less.
+    threshold_low = None
+    threshold_high = None
+    target_maximum = 82
+    target_minimum = 40
+    crossed_below_low_threshold_at = None
+    crossed_above_high_threshold_at = None
+    threshold_time_delay = 5 * 60 # 5 minutes
 
-    def __init__(self, _heater, thermometer, clock=None):
+    def __init__(self, heater=HeaterControl(), thermometer=None, clock=Clock()):
+        self.heater = heater
+        self.clock = clock
         self.thermometer = thermometer
-        self.available_modes = ['off', 'on', 'auto']
-        self.current_mode = 'off'
-
-        if _heater:
-            self.heater = _heater
-        else:
-            self.heater = HeaterControl()
-
-        if clock:
-            self.clock = clock
-        else:
-            self.clock = Clock()
-
-        self.threshold_low = None
-        self.target_temp = None
-        self.threshold_high = None
 
     def iterate(self):
-        room_temp = self.get_room_temperature()
-        if room_temp < self.threshold_low:
-            self.heater.set_to_on(True)
-        if room_temp > self.threshold_high:
-            self.heater.set_to_on(False)
+
+        # report
+        self._logger.debug("starting iterate() - mode={m}, heater={h}, temp={t}, targets={x}, clock={c}".format( \
+            m=self.current_mode,
+            h="on" if self.get_heater_is_on() else "off",
+            t=self.get_room_temperature(),
+            x="{}/{}/{}".format(self.threshold_low, self.target_temp, self.threshold_high),
+            c=self.clock.time()
+        ))
+
+        self.check_thresholds()
+
+        if self.current_mode == "target":
+            # if we've been below threshold and...
+            #    the heater isn't currently on and...
+            #    we've been below threshold long enough
+            # then turn on heater
+            if self.crossed_below_low_threshold_at is not None and \
+                not self.get_heater_is_on() and \
+                    (self.clock.time() >= (self.crossed_below_low_threshold_at + self.threshold_time_delay)):
+                    self._logger.info("Too long below threshold, turning on heater")
+                    self.heater.set_to_on(True)
+
+            # opposite above for turning off heater
+            if self.crossed_above_high_threshold_at is not None and \
+                self.get_heater_is_on() and \
+                    (self.clock.time() >= (self.crossed_above_high_threshold_at + self.threshold_time_delay)):
+                    self._logger.info("Too long above threshold, turning off heater")
+                    self.heater.set_to_on(False)
+
+        # iterate the heater
         self.heater.iterate()
 
     def get_heater_is_on(self):
@@ -35,18 +64,57 @@ class Thermostat(object):
     def set_mode(self, mode):
         if mode not in self.available_modes:
             raise ThermostatException("Unrecognized mode: {}".format(mode))
-        self.current_mode = mode
 
-    def set_heater_to_on(self, val):
-        pass
+        if mode == "on":
+            self.heater.set_to_on(True)
+            self.heater.iterate()
+
+        if mode == "off":
+            self.heater.set_to_on(False)
+            self.heater.iterate()
+
+        if mode == "target":
+            pass
+
+        self.current_mode = mode
+        self.iterate()
 
     def get_room_temperature(self):
         return self.thermometer.temperature
 
     def set_target_temperature(self, temp):
+        if temp > self.target_maximum:
+            raise ThermostatException("Can not set target above limit of {}".format(self.target_maximum))
+        if temp < self.target_minimum:
+            raise ThermostatException("Can not set target below limit of {}".format(self.target_minimum))
         self.target_temp = temp
         self.threshold_low = temp - 1
         self.threshold_high = temp + 1
 
     def get_target_temperature(self):
         return self.target_temp
+
+    def check_thresholds(self):
+        # record threshold crossings
+        if self.threshold_low is not None:
+            # if below low threshold
+            if self.thermometer.temperature < self.threshold_low:
+                if self.crossed_below_low_threshold_at is None:
+                    self.crossed_below_low_threshold_at = self.clock.time()
+                    self._logger.debug("Exceeded low threshold")
+            # if at or above low threshold
+            else:
+                if self.crossed_below_low_threshold_at is not None:
+                    self.crossed_below_low_threshold_at = None
+                    self._logger.debug("Within low threshold");
+        if self.threshold_high is not None:
+            # if above high threshold
+            if self.thermometer.temperature > self.threshold_high:
+                if self.crossed_above_high_threshold_at is None:
+                    self.crossed_above_high_threshold_at = self.clock.time()
+                    self._logger.debug("Exceeded high threshold")
+            # if at or below high threshold
+            else:
+                if self.crossed_above_high_threshold_at is not None:
+                    self.crossed_above_high_threshold_at = None
+                    self._logger.debug("Within high threshold")
