b9ba29afde7cd761e74145299485f315f0850ae6
Mike Claffey <mikeclaffey@yahoo.com>
2016-04-07 19:53:35 -0400

    room temp - always include most recent reading, even if diff.abs is small

--- web/prep_web_data.py
+++ web/prep_web_data.py
@@ -10,9 +10,10 @@ room = pd.read_sql("SELECT read_time as date, temperature as temp FROM temp", db
 
 # remove rows that don't differ by more than 0.5 degrees
 i_is_diff = (room.temp.diff().abs() > 0)
+i_is_diff.iloc[-1] = True # always keep the last record
 room = room[i_is_diff]
 print "Kept values: {}, {:%}".format( room.shape[0], i_is_diff.mean())
-
+print room.tail()
 room.to_csv("public_html/room.tsv", sep="\t", index=False)
 
 # #########################
@@ -20,3 +21,4 @@ room.to_csv("public_html/room.tsv", sep="\t", index=False)
 db_weather = sqlite3.connect("../data/weather.db")
 weather = pd.read_sql("SELECT datetime as date, temperature_f as temp FROM readings ORDER BY datetime", db_weather)
 weather.to_csv("public_html/weather.tsv", sep="\t", index=False)
+print weather.tail()
